91ea5e58d791faaa7d9dd985b9c3a8041e024355
Furkan Aydemir <152931069+deerborg@users.noreply.github.com>
2024-02-14 00:14:14 +0300

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -5,9 +5,9 @@
  */
 public class Tester {
     public static void main(String[] args) {
-        
-        
+        //Examine account.java 
         Account newAccount = new Account();
+         // It can perform all operations in version 1.0 with a single called object.
         
         
         
